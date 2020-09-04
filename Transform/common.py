444b9c5942da123fbd4e92fd1e562e3707638d5e
fabio-gz <fabiogomez.silva@gmail.com>
2020-09-03 20:41:47 -0500

    [add]first xpath

---
+++ Transform/common.py
@@ -0,0 +1,13 @@
+# cargar yaml
+import yaml
+#global var
+__config = None
+
+
+def config():
+    global __config
+    if not __config:
+        with open('config.yml', mode='r') as f:
+            __config = yaml.safe_load(f)
+
+    return __config
